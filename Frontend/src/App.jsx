4c9505c900f7649f95a54524051d297a36742227
Tarun Sudhir Behera <99060004+Tarun-032@users.noreply.github.com>
2024-09-15 21:08:24 +0530

    3:23:26 iske baad backend hai

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -5,7 +5,7 @@ import Home from './pages/Home/Home'
 import Cart from './pages/Cart/Cart'
 import PlaceOrder from './pages/PlaceOrder/PlaceOrder'
 import Footer from './components/Footer/Footer'
-import LoginPopup from './components/LoginPopup/LoginPopup'
+import LoginPopup from './Components/LoginPopup/LoginPopup'
 
 const App = () => {
 
@@ -19,7 +19,7 @@ const App = () => {
        <Routes>
          <Route path='/' element={<Home/>} />
          <Route path='/cart' element={<Cart/>} />
-         <Route path='/PlaceOrder' element={<PlaceOrder/>} />
+         <Route path='/order' element={<PlaceOrder/>} />
        </Routes>
      </div>
      <Footer />
