cc5223552dfd9e4be5bd1b33bf98bf2ce1f15f85
Tarun Sudhir Behera <99060004+Tarun-032@users.noreply.github.com>
2024-09-15 17:47:04 +0530

    exact 2:25:20 tak

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -8,11 +8,12 @@ import Footer from './components/Footer/Footer'
 import LoginPopup from './components/LoginPopup/LoginPopup'
 
 const App = () => {
-const [showLogin,setShowLogin] = useState(false)
+
+  const [showLogin,setShowLogin] = React.useState(false)
 
   return (
     <>
-    {showLogin?<LoginPopup/>:<></>}
+    {showLogin?<LoginPopup setShowLogin={setShowLogin}/> :<></>}
      <div className='app'>
        <Navbar setShowLogin={setShowLogin}/>
        <Routes>
--- Frontend/src/Components/LoginPopup/LoginPopup.jsx
+++ Frontend/src/Components/LoginPopup/LoginPopup.jsx
@@ -1,11 +1,32 @@
 import React, { useState } from 'react'
 import './LoginPopup.css'
+import { assets } from '../../assets/assets'
 
-const LoginPopup = () => {
+const LoginPopup = ({setShowLogin}) => {
+
+  const [currState,setCurrState] = React.useState("Login")
 
   return (
     <div className='login-popup'>
-      Login
+      <form className='login-popup-container'>
+        <div className="login-popup-title">
+          <h2>{currState}</h2>
+          <img onClick={()=>setShowLogin(false)} src={assets.cross_icon} alt="" />
+
+        </div>
+        <div className="login-popup-inputs">
+          {currState==="Login"?<></>:<input type="text" placeholder= 'Your name' required></input>}
+          <input type="email" placeholder='Your Email'required ></input>
+          <input type="password" placeholder='Password'required ></input> 
+        </div>
+        <button>{currState==="Sign Up"?"Create account":"Login"}</button>
+        <div className="login-popup-condition">
+          <input type="checkbox" required></input>
+          <p>by contnuing, i agree to the terms and condition of use & privacy policy</p>
+        </div>
+
+      </form>
+      
     </div>
   )
 }
