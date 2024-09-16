98a24850890ca92701d1a8516aa274db7497e049
Tarun Sudhir Behera <99060004+Tarun-032@users.noreply.github.com>
2024-09-16 20:41:12 +0530

    7:18:51 scene hora wo thunder client ka jaara nai data base mai

---
+++ Backend/controllers/cartController.js
@@ -0,0 +1,36 @@
+import userModel from "../models/userModel.js";
+
+// add item to user cart 
+const addToCart =  async (req,res) => {
+    try {
+        let userData = await userModel.findOne({_id:req.body.userId});
+        let cartData = await userData.cartData;
+        if(!cartData[req.body.itemId]) 
+        {
+                cartData[req.body.itemId] = 1;
+
+        } 
+        else{
+            cartData[req.body.itemId] += 1;
+        }
+        await userModel.findOneAndUpdate(req.body.userId, {cartData});
+        res.json({success:true, message:"Added to cart"});
+
+         } catch (error) {
+            console.log(error);
+            res.json({success:false, message:"Error"});
+        
+    }   
+}
+
+//remove carts 
+const removeFromCart = async (req,res) => {
+
+}
+
+// fetch user cart data 
+const getCart = async (req,res) => {
+    
+}
+
+export {addToCart,removeFromCart,getCart}
